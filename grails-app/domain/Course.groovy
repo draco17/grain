409f3a9b5da6f26402fc8bfd53d6930946cf1d82
Domingo Suárez Torres <domingo.suarez@gmail.com>
2009-01-29 22:44:22 -0600

    added Course properties and added test case

--- grails-app/domain/Course.groovy
+++ grails-app/domain/Course.groovy
@@ -1,3 +1,9 @@
 class Course {
-
+	String name
+	String description
+	
+	static constraints = {
+		name(blank:false,nullable:false,size:1..50)
+		name(blank:false,nullable:false,size:1..10000)
+	}
 }
--- test/unit/CourseTests.groovy
+++ test/unit/CourseTests.groovy
@@ -1,8 +1,15 @@
 import grails.test.*
 
 class CourseTests extends grails.test.GrailsUnitTestCase {
-
-    void testSomething() {
-
-    }
+	void testBlankName() {
+		mockDomain(Course)
+		def course = new Course(name:"")
+		assertFalse 'the name can not be blank', course.validate()
+	}
+	
+	void testNullName() {
+		mockDomain(Course)
+		def course = new Course()
+		assertFalse 'the name can not be null', course.validate()
+	}
 }
