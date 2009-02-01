1050e99a9389b4fd6786d7baa4d780e2da3e9178
Domingo Suarez Torres <domingo.suarez@gmail.com>
2009-02-01 00:15:19 -0600

    corrected test cases

--- test/unit/com/grainapp/domain/CourseTests.groovy
+++ test/unit/com/grainapp/domain/CourseTests.groovy
@@ -13,7 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.grainapp.domain
+
 import grails.test.*
+import com.grainapp.domain.*
 
 class CourseTests extends grails.test.GrailsUnitTestCase {
 	void testBlankName() {
